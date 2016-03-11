ae2d54f4221b5f25f87d994d33ccfe0e7e8a109b
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-03-11 11:45:32 -0500

    Update API

--- src/main/scala/ext/cds2/modules/ModuleTemplate.scala
+++ src/main/scala/ext/cds2/modules/ModuleTemplate.scala
@@ -1,6 +1,7 @@
 package ext.cds2.modules
 
-import nasa.nccs.cdapi.kernels.{ KernelModule, Kernel, ExecutionResult, Port, DataFragment }
+import nasa.nccs.cdapi.cdm.{KernelDataInput, BinnedArrayFactory, aveSliceAccumulator, PartitionedFragment}
+import nasa.nccs.cdapi.kernels._
 
 /**
   * Created by tpmaxwel on 2/9/16.
@@ -16,7 +17,12 @@ class ModuleTemplate extends KernelModule {
     val outputs = List(Port("result", "1"))
     override val description = "Average over Input Fragment"
 
-    def execute(inputSubsets: List[DataFragment], run_args: Map[String, Any]): ExecutionResult = {
+    def execute(context: ExecutionContext ): ExecutionResult = {
+      val inputSubsets: List[KernelDataInput] =  context.fragments
+      val optargs: Map[String,String] =  context.args
+      val input_array = inputSubsets(0).dataFragment.data
+      val axisSpecs = inputSubsets(0).axisSpecs
+      val axes = axisSpecs.getAxes
       val inputSubset = inputSubsets.head
       val result = "0.0"  // Add computation here
       logger.info("Kernel %s: Executed operation %s, result = %s ".format( name, operation, result ))
